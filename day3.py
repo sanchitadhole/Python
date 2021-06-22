bdb9c41a1c3977c3e16853c1e7af54f993831b41
sanchita <sanchitadhole27@gmail.com>
2021-06-22 10:13:20 +0530

    first commit

---
+++ day3.py
@@ -0,0 +1,35 @@
+# # Factorial program
+# num = int(input('enter a number'))
+# fact = 1
+# if num > 0:
+#     for i in range(1,num+1):
+#         fact = fact * i
+#     print('factorial of:', num ,"is", fact)
+#
+
+# prime number
+# num = int(input('enter a number'))
+# if num>1:
+#     for i in range(2,num):
+#         if(num % i) == 0:
+#             print(num," is a not prime number")
+#             break
+#     else:
+#         print(num,'number is prime number')
+
+
+
+
+# for letter in 'python':
+#     print('current letter:', letter)
+
+# ******************
+# For loops
+# fruits = ['apple','banana','mango','straberry']
+# for i in fruits:
+#     print('current fruits:',fruits)
+# print(fruits[0])
+# fruits[1] = 'coconut'
+# print(fruits)
+#
+# # *********************
---
+++ day4.py
@@ -0,0 +1,41 @@
+# Control statements/ Conditional statements
+
+# if statement
+#
+# x = 2
+#
+# if x==1:
+#     print('one')
+# if x==2:
+#     print('two')
+# if x ==3:
+#     print('three')
+# if x == 4:
+#     print('four')
+
+# **************************
+# if -- else statement
+
+x = 7
+r = x%2
+
+if r==0:
+    print('even')
+else:
+    print('odd')
+
+# if-elif-else statement
+
+x = 5
+
+if x==1:
+    print('one')
+elif x==2:
+    print('two')
+elif x ==3:
+    print('three')
+elif x == 4:
+    print('four')
+else:
+    print('invalid number')
+
